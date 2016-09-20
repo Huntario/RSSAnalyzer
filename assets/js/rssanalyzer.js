779cbd3e11a828c00bd85ae9b8b8ce716e77ee0a
Hunter Leachman <leachmanh@Hunters-MacBook-Air.local>
2016-09-19 21:22:55 -0500

    experiemneted with ways to feed data into HP API

--- assets/js/rssanalyzer.js
+++ assets/js/rssanalyzer.js
@@ -2,14 +2,12 @@
 $('.btn btn-primary').on('click', function () {
     $(this).button('toggle') // button text will be "finished!"
   })
-
 //Upon clicking the "analyze" button, we get the chosen RSS feedl link and send it it RSStoJSON
 
 
 //HERE IS RSS TO JSON CONVERTER
 $('button').on('click', function() {
 
-        var testLink = "https%3A%2F%2Fnews.ycombinator.com%2Frss"
         var rsslink = $(this).data('link');
         //var queryURL = "http://rss2json.com/api.json?rss_url=" + "http://feeds.bbci.co.uk/news/world/rss.xml?edition=uk" ;
         var queryURL = "http://rss2json.com/api.json?rss_url=" + rsslink;
@@ -19,8 +17,12 @@ $('button').on('click', function() {
                 method: 'GET'
             })
             .done(function(response) {
-
                 console.log(response);
+                for (var i = 0; i < response.items.length; i++){
+                var l = response.items[i].title
+                analysis(l); 
+
+                }
                 })
         });
 
@@ -30,18 +32,16 @@ $('button').on('click', function() {
 
 
 //HERE IS THE ANALYZER FROM HEWELET PACKARD ENT
-$('button').on('click', function() {
+function analysis(l) {
 
-        var rsslink = $(this).data('link');
-        //var queryURL = "http://rss2json.com/api.json?rss_url=" + "http://feeds.bbci.co.uk/news/world/rss.xml?edition=uk" ;
-        var queryURL = "https://api.havenondemand.com/1/api/sync/analyzesentiment/v1?text=http%3A%2F%2Fwww.cnn.com&apikey=ba67a893-398a-4cdb-ac52-57764039436f";
-   
+        var titleText = l.split(' ').join('+');
+        var queryURL = "https://api.havenondemand.com/1/api/sync/analyzesentiment/v1?text=http%3A%2F%2F" + titleText + "&apikey=ba67a893-398a-4cdb-ac52-57764039436f";
         $.ajax({
                 url: queryURL,
                 method: 'GET'
             })
             .done(function(response) {
-
+                console.log ("titleTex = " + titleText)
                 console.log(response);
                 })
-        });
+        };
